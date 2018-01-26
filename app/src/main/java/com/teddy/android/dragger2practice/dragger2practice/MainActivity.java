225a724e9ca2aec3259911212b0c48900fb3ac4a
Moon teayong <moonny76@gmail.com>
2018-01-26 15:13:55 +0900

    Add default application

---
+++ app/src/main/java/com/teddy/android/dragger2practice/dragger2practice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teddy.android.dragger2practice.dragger2practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
